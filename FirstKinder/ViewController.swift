9e642fdca4a9293fa42726256e16a6961de9bc5e
Haedong-Jeon <haedongjeon@icloud.com>
2020-11-30 09:18:40 +0900

    Initial Commit

---
+++ FirstKinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKinder
+//
+//  Created by 전해동 on 2020/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
