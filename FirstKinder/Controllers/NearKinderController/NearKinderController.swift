5d0dcc19e9a06fef545359e33a9f7c5f64be8c15
Haedong-Jeon <haedongjeon@icloud.com>
2020-12-03 16:07:06 +0900

    근처 어린이집 표시 완료

--- FirstKinder/Controllers/NearKinderController/NearKinderController.swift
+++ FirstKinder/Controllers/NearKinderController/NearKinderController.swift
@@ -60,6 +60,7 @@ class NearKinderController: UIViewController, MKMapViewDelegate {
             mapView.showsUserLocation = true
             centerViewOnUserLocation()
             locationManager.startUpdatingLocation()
+            kinders.forEach({setAnnotaion(kinder: $0)})
             break
         @unknown default:
             break
@@ -74,6 +75,17 @@ class NearKinderController: UIViewController, MKMapViewDelegate {
     func setUpLocationManager() {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    func setAnnotaion(kinder: Kinder) {
+        let annotation = MKPointAnnotation()
+        guard let lo = Double(kinder.lo) else { return }
+        guard let la = Double(kinder.la) else { return }
+        
+        let centerCoordinate = CLLocationCoordinate2D(latitude: la, longitude: lo)
+        annotation.coordinate = centerCoordinate
+        annotation.title = kinder.title
+        mapView.addAnnotation(annotation)
+        mapView.setRegion(MKCoordinateRegion(center: centerCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)), animated: false)
     }
 }
 
