1124aca223833183d13d14c8955f77dad272b590
Haedong-Jeon <haedongjeon@icloud.com>
2020-12-03 16:18:21 +0900

    근처 어린이집 찾기 기능 완성

--- FirstKinder/Controllers/NearKinderController/NearKinderController.swift
+++ FirstKinder/Controllers/NearKinderController/NearKinderController.swift
@@ -88,23 +88,25 @@ class NearKinderController: UIViewController, MKMapViewDelegate {
         mapView.setRegion(MKCoordinateRegion(center: centerCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)), animated: false)
     }
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        var selectedKinder: Kinder?
         if let annotationTitle = view.annotation?.title {
             kinders.forEach({
                 if $0.title == annotationTitle {
-                    let detailController = DetailController()
-                    detailController.kinder = $0
-                    
-                    let transition = CATransition()
-                    transition.duration = 0.3
-                    transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
-                    transition.type = .fade
-
-                    self.navigationController?.view.layer.add(transition, forKey: nil)
-
-                    self.navigationController?.pushViewController(detailController, animated: false)
-                    return
+                    selectedKinder = $0
                 }
             })
+            let detailController = DetailController()
+            detailController.kinder = selectedKinder!
+            
+            let transition = CATransition()
+            transition.duration = 0.3
+            transition.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+            transition.type = .fade
+            
+            self.navigationController?.view.layer.add(transition, forKey: nil)
+            
+            self.navigationController?.pushViewController(detailController, animated: false)
+            return
         }
     }
 }
