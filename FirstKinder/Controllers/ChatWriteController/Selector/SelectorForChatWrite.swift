c49ff6d3eff75ebe1992d33b86f4915c43331881
Haedong-Jeon <haedongjeon@icloud.com>
2020-12-08 18:48:06 +0900

    파이어베이스 연동

--- FirstKinder/Controllers/ChatWriteController/Selector/SelectorForChatWrite.swift
+++ FirstKinder/Controllers/ChatWriteController/Selector/SelectorForChatWrite.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 extension ChatWriteController {
     @objc func keyBoardCameraButtonTap() {
@@ -26,6 +27,30 @@ extension ChatWriteController {
         }
     }
     @objc func handleUpload() {
+        if chatBodyTextView.text.isEmpty { return }
+        upload {
+            self.chatBodyTextView.text.removeAll()
+            let showSuccess = UIAlertController(title: "업로드", message: "업로드 됐어요!", preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "확인", style: .default) { ACTION in
+                self.navigationController?.popViewController(animated: true)
+            }
+            showSuccess.addAction(okButton)
+            self.present(showSuccess, animated: true, completion: nil)
+        }
+    }
+    
+    func upload(completion: @escaping() -> Void) {
+        let uid = NSUUID().uuidString
+        let value: [String: Any] = ["uid": uid, "chat": chatBodyTextView.text]
         
+        DB_CHATS.child(uid).updateChildValues(value) { (error, ref) in
+            if error != nil {
+                let showError = UIAlertController(title: "업로드", message: "에러가 발생했어요 ㅜ.ㅜ", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "확인", style: .default, handler: nil)
+                showError.addAction(okButton)
+                self.present(showError, animated: true, completion: nil)
+            }
+            completion()
+        }
     }
 }
