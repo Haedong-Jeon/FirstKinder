3c7e3ae79569b83ff027461a0c775be3b09d37df
Haedong-Jeon <haedongjeon@icloud.com>
2020-12-12 10:06:15 +0900

    아이콘 변경

--- FirstKinder/Controllers/MainController/MainController.swift
+++ FirstKinder/Controllers/MainController/MainController.swift
@@ -21,24 +21,24 @@ class MainController: UITabBarController, UITabBarControllerDelegate {
     }
     func setTabs() {
         let kinderListController = ListController(collectionViewLayout: UICollectionViewFlowLayout())
-        kinderListController.tabBarItem.image = #imageLiteral(resourceName: "list-1")
+        kinderListController.tabBarItem.image = #imageLiteral(resourceName: "to-do-list (1)")
         kinderListController.tabBarItem.title = "어린이집 목록"
         
         let nearKinderController = NearKinderController()
-        nearKinderController.tabBarItem.image = #imageLiteral(resourceName: "map (1)")
+        nearKinderController.tabBarItem.image = #imageLiteral(resourceName: "maps-and-flags (1)")
         nearKinderController.tabBarItem.title = "근처 어린이집"
         
         let parentsChatController = ChatController(collectionViewLayout: UICollectionViewFlowLayout())
-        parentsChatController.tabBarItem.image = #imageLiteral(resourceName: "chat (1)")
+        parentsChatController.tabBarItem.image = #imageLiteral(resourceName: "chat-bubbles-with-ellipsis (1)")
         parentsChatController.tabBarItem.title = "이야기"
         
         let myKindersController = MyKindersController(collectionViewLayout: UICollectionViewFlowLayout())
-        myKindersController.tabBarItem.image = #imageLiteral(resourceName: "like")
+        myKindersController.tabBarItem.image = #imageLiteral(resourceName: "like (2)")
         myKindersController.tabBarItem.title = "관심 어린이집"
         
         let controllers = [kinderListController, nearKinderController, parentsChatController, myKindersController]
         viewControllers = controllers.map({UINavigationController(rootViewController: $0)})//탭안의 뷰컨트롤러들에게 네비 바 붙이기.
-        
+        tabBar.tintColor = #colorLiteral(red: 0.1884440184, green: 0.1871832013, blue: 0.2520470917, alpha: 1)
         tabBar.isTranslucent = false
     }
 }
